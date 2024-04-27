c0978774a95b85540629349fab2f1cd3116b282b
Ha Quang Minh <haquangminhbnvn@gmail.com>
2024-04-27 11:07:47 +0700

    encode decode message

--- cmd/client.ts
+++ cmd/client.ts
@@ -1,7 +1,12 @@
 import * as grpc from "grpc";
-import { protocolService } from "../types/grpc"; // Assuming you have generated types from the .proto file
-import { BaseDecoder, BaseEncoder } from "../utils";
 import * as protoLoader from "@grpc/proto-loader";
+import {
+  Message,
+  MessageEnvelope,
+  MessageType,
+  decodeMessageEnvelope,
+  encodeMessageEnvelope,
+} from "../types";
 
 const PROTO_PATH = "hihi/hihi.proto";
 
@@ -16,25 +21,39 @@ const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
 
 // Load the service definition
 const protoDescriptor = grpc.loadPackageDefinition(packageDefinition);
-const greeterService = protoDescriptor["Hihi"] as any;
+const hihiService = protoDescriptor["Hihi"] as any;
 
 // Create a new client instance
-const client = new greeterService(
-  "localhost:50051",
+const client = new hihiService(
+  "localhost:0",
   grpc.credentials.createInsecure()
 );
 
 // Function to make a request to the server
-function greet(): void {
-  const request = { blob: BaseEncoder.encodeUnit64(12n) };
+function hihi(messageEnvelope: MessageEnvelope): void {
+  const request = { blob: encodeMessageEnvelope(messageEnvelope) };
 
   client.sayHi(request, (error: grpc.ServiceError | null, response: any) => {
     if (error) {
       console.error("Error:", error.message);
       return;
     }
-    console.log("Response:", BaseDecoder.decodeUnit64(response.blob));
+    console.log(
+      "Response:",
+      JSON.stringify(decodeMessageEnvelope(response.blob))
+    );
   });
 }
 
-greet();
+const message: Message = {
+  type: MessageType.HELLO_ACK_MESSAGE_TYPE,
+  nonce: Buffer.from("hihi", "utf-8"),
+};
+const messageEnvelope: MessageEnvelope = {
+  magic: 696969,
+  timestamp: Math.floor(Date.now() / 1000),
+  message: message,
+  signature: Buffer.from("hihihihihihihihi", "utf-8"),
+};
+
+hihi(messageEnvelope);
--- cmd/server.ts
+++ cmd/server.ts
@@ -1,30 +1,40 @@
 import * as grpc from "grpc";
 import { protocolService } from "../types/grpc";
-import { BaseDecoder, BaseEncoder } from "../utils";
+import {
+  decodeMessageEnvelope,
+  encodeMessageEnvelope,
+} from "../types/protocol";
 
 function sayHi(
   call: grpc.ServerUnaryCall<any>,
   callback: grpc.sendUnaryData<any>
 ): void {
   const blob = call.request.blob;
-  console.log("receive data", BaseDecoder.decodeUnit64(blob));
-  // decode message
+  const [messageEnvelope, remainingBuffer] = decodeMessageEnvelope(blob);
+  if (remainingBuffer.length !== 0) {
+    throw Error("Invalid message type");
+  }
   // handle logic
-  // encode responce
-  const response = { blob: BaseEncoder.encodeUnit64(23n) };
+
+  console.log("Response:", JSON.stringify(decodeMessageEnvelope(blob)));
+
+  const response = { blob: encodeMessageEnvelope(messageEnvelope) };
   callback(null, response);
 }
 function main() {
+  const port = process.argv[2];
+  const host = `0.0.0.0:${port}`;
   const server = new grpc.Server();
   server.addService(protocolService, { sayHi });
   server.bindAsync(
-    "127.0.0.1:50051",
+    host,
     grpc.ServerCredentials.createInsecure(),
     (err, port) => {
       if (err) {
         console.error("Server bind failed:", err);
         return;
       }
+      console.log(port);
       console.log(`Server is listening on 127.0.0.1:${port}`);
       server.start();
     }
